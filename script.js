b25c7556c0e9a21bfee1fba69a852de751704fa9
vertvert123 <vertvert123@naver.com>
2021-12-07 17:41:19 +0900

    git test commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/*const c = 5;
+const d = 7;
+const e = ["a","b","c"];
+const calculator ={
+    add : function (a,b) {
+        return(a+b);
+    },
+    name : "star"
+    
+    
+};
+
+function power(a,b) {
+    return(a**b);
+}
+const human = {
+    body : "big",
+    hand : "small",
+    leg : "long"
+}
+const value = calculator.add(c,d);
+console.log(calculator.name);
+console.log(value);
+const value2 = power(c,d);
+console.log(value2);
+console.log(human)
+console.log(e[1]);
+const age = parseInt(prompt("write your age"));
+if(isNaN(age)){
+    alert("다시 입력하세요");
+}
+else{
+alert("your age is"+age);
+}
+
+*/
+const title = document.getElementById("title")
+title.innerText = "hi!";
