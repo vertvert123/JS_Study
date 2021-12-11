9cc9dde48bdbd7f9274d90528543d222c0b80484
vertvert123 <vertvert123@naver.com>
2021-12-12 00:10:00 +0900

    5장끝

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-   const date = new Date();
-   clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
- }
+    const date = new Date();
+    let hour = String(date.getHours()).padStart(2, "0");
+    let minute = String(date.getMinutes()).padStart(2, "0")
+    let second = String(date.getSeconds()).padStart(2, "0")
+    clock.innerText = `${hour}:${minute}:${second}`;
+}
 
 setInterval(getClock, 1000) // 실행할 함수, 1000 = 1초 정한 시간마다 함수를 실행한다.
