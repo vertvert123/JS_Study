6419ce4989894842d34e3ae10b226105db8643ee
vertvert123 <vertvert123@naver.com>
2021-12-13 19:19:47 +0900

    delete todo

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const todoList = document.getElementById("todo-list");
 
+function deletedoto(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deletedoto);
     li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newTodo;
     todoList.appendChild(li);
 
